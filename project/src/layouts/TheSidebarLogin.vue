ff01f3228d5d4f8f8b4089f92d72a4ea8bc4f9f1
LJYGALAXYBOOK\ptljo <ptljoo1572@gmail.com>
2025-04-08 20:39:18 +0900

    작업 내용
    nav bar outline 레이아웃 결정
    
    로고사진 / 프로필사진 / 재정요약,거래내역조회,마이페이지,로그아웃 버튼들 sizing
    
    nav bar 내의 요소들 positioning

--- project/src/layouts/TheSidebarLogin.vue
+++ project/src/layouts/TheSidebarLogin.vue
@@ -3,37 +3,78 @@
 <template>
   <div :class="navClass">
     <nav id="nav">
-      <h1><img src="" alt="사이트로고"></h1>
-      <div><img src="" alt="사용자프로필"></div>
-      <div><span>000님</span></div>
+      <h1><img src="https://placehold.co/160" alt="사이트로고" /></h1>
+      <div class="nav-bar-profile">
+        <img src="https://placehold.co/160" alt="사용자프로필" />
+        <span class="nav-bar-userName">000님</span>
+      </div>
       <div class="nav-bar-btnbox">
         <button class="nav-bar-btn">재정요약</button>
         <button class="nav-bar-btn">거래내역조회</button>
         <button class="nav-bar-btn">마이페이지</button>
+        <button class="nav-bar-btn">로그아웃</button>
       </div>
     </nav>
   </div>
 </template>
 
 <style>
-#nav{
-  display: flex;  /* 1.레이아웃 */
-  flex-direction:column;
+
+#nav {
+  display: flex; /* 1.레이아웃 */
+  flex-direction: column;
   
+
   width: 18.75rem; /* 2.BOX */
   height: 64rem;
-  
-  color: var(--color-purple); /* 3.배경 */
 
+  color: var(--color-purple); /* 3.배경 */
 }
-#nav .nav-bar-btnbox{
+#nav,
+#nav > * {
+  align-items: center;
+  text-align: center;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+h1 {
+  margin-top: 4.125rem;
+}
+h1 > img {
+  width: 10rem;
+  height: 10rem;
+}
+.nav-bar-profile {
+  width: 10rem;
+  height: 10rem;
+  margin-top: 2.5rem;
+  margin-bottom: 8.875rem;
+}
+.nav-bar-userName{
+  display: block;
+  line-height: 44px;
+
+  font-size: 15px;
+  
+  
+}
+#nav .nav-bar-btnbox {
   display: flex;
   flex-direction: column;
+  margin-bottom: 3.875rem;
 }
-.nav-bar-btn{
-  width: 13.125rem; /* 210px */
-  height: 4.25rem; /* 68px */
-  border-radius: 30px;
+.nav-bar-btn {
+  width: 13.125rem;      /* 210px */
+  height: 4.25rem;       /* 68px */
+  border-radius: 1.875rem;  /* 30px */
+  padding: 0 2.5rem;    /* 40px */
+  margin-bottom: 1.625rem;
+  align-items: center;
 }
-
+.nav-bar-btnbox .nav-bar-btn:nth-child(4){
+  margin-bottom: 0;
+}
+.nav-bar-btnbox .nav-bar-btn:nth-child(1){
+  margin-top: 2.25rem;   /* 36px */
+ 
+}
 </style>
