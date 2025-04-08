c4c98579c505f8c565ae9b5505f424051c528b13
Mminjae <alswo2833@naver.com>
2025-04-08 17:47:16 +0900

    feat: 세부 거래내역 생성_모달
    - 세부 거래내역 생성_모달 디자인 틀만 완료

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <TheSidebar />
+  <ModalDetailPost />
+  <RouterView />
 </template>
 <script>
 import TheSidebar from '@/layouts/TheSidebar.vue'
+import ModalDetailPost from '@/components/modal/ModalDetailPost.vue'
 export default {
   name: 'App',
-  components: { TheSidebar },
+  components: { TheSidebar, ModalDetailPost },
 }
 </script>
--- project/src/components/modal/ModalAddPost.vue
+++ project/src/components/modal/ModalAddPost.vue
@@ -1,5 +1,7 @@
-<script></script>
 <template>
   <h1>내역 추가 모달 (page 8)</h1>
 </template>
-<style></style>
+
+<script setup></script>
+
+<style scoped></style>
