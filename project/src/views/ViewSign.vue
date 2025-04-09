4ccc7cce5e772978f0ef7411388266531e8992ee
woojeong98 <wooo9806@gmail.com>
2025-04-09 17:41:22 +0900

    feature: 회원가입 로그인(#34)

--- project/src/views/ViewSign.vue
+++ project/src/views/ViewSign.vue
@@ -1,7 +1,24 @@
-<script></script>
-
 <template>
-  <h1>회원가입</h1>
+  <div id="join">
+    <h3 id="joinHeader">회원가입</h3>
+    <form @submit.prevent="submitForm">
+      <!-- 이름 -->
+      <div>
+        <label class="joinLabel">이름</label>
+        <input v-model="form.name" type="text" class="joinBox" />
+      </div>
+    </form>
+  </div>
 </template>
 
-<style></style>
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+  name: ''
+})
+
+const submitForm = () => {
+  console.log('제출됨:', form.name)
+}
+</script>
