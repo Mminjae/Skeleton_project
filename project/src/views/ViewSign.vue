8b5f4bc08664029e2d23ecf67a00e96b8db0d606
woojeong98 <wooo9806@gmail.com>
2025-04-09 19:50:07 +0900

    feature: 전화번호 입력창 및 유효성 검사

--- project/src/views/ViewSign.vue
+++ project/src/views/ViewSign.vue
@@ -2,12 +2,18 @@
   <div id="join">
     <h3 id="joinHeader">회원가입</h3>
     <form @submit.prevent="submitForm">
-      <!-- 이름 -->
+      <!-- 이름 입력창 -->
       <div>
         <label class="joinLabel">이름</label>
         <input v-model="form.name" type="text" class="joinBox" />
       </div>
-      <button type="submit">회원가입</button>
+      <!-- 휴대폰 번호 입력창 -->
+      <div>
+        <label class="joinLabel">핸드폰 번호</label>
+        <input v-model="form.phone" type="text" @blur="validatePhone" class="joinBox" />
+        <span v-if="errors.phone">{{ errors.phone }}</span>
+      </div>
+      <button type="joinSubmit">회원가입</button>
     </form>
   </div>
 </template>
@@ -16,8 +22,18 @@
 import { reactive } from 'vue'
 
 const form = reactive({
-  name: ''
+  name: '',
+  phone: ''
 })
+
+const errors = reactive({
+  phone: ''
+})
+
+const validatePhone = () => {
+  const regex = /^[0-9]{10,11}$/
+  errors.phone = regex.test(form.phone) ? '' : '숫자만 입력해주세요 (10~11자리)'
+}
 
 const submitForm = () => {
   console.log('제출됨:', form.name)
