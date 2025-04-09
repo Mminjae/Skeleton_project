89b4685f7a9f43c24535a39c5b8c4e24f7bc87f2
LJYGALAXYBOOK\ptljo <ptljoo1572@gmail.com>
2025-04-09 13:16:43 +0900

    [임시] 로컬호스트로 보기위해 app.vue에 import, mount

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,15 +1,17 @@
 <template>
+  <ModalFilter/>
   <!-- <TheSidebar /> -->
   <!-- <ModalDetailPost /> -->
-  <TheSidebarLogin/>
+  <!-- <TheSidebarLogin/> -->
   <RouterView />
 </template>
 <script>
 import TheSidebar from '@/layouts/TheSidebar.vue'
 import ModalDetailPost from '@/components/modal/ModalDetailPost.vue'
 import TheSidebarLogin from '@/layouts/TheSidebarLogin.vue'
+import ModalFilter from './components/modal/ModalFilter.vue'
 export default {
   name: 'App',
-  components: { TheSidebar, ModalDetailPost,TheSidebarLogin },
+  components: { TheSidebar, ModalDetailPost, ModalFilter, TheSidebarLogin },
 }
 </script>
