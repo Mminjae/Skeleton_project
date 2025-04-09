25b3596f7971af7c5662f5bea2055e19bae84785
woojeong98 <wooo9806@gmail.com>
2025-04-10 07:32:06 +0900

    feature: 아이디 입력 및 유효성 검사

--- project/src/views/ViewSign.vue
+++ project/src/views/ViewSign.vue
@@ -13,7 +13,13 @@
         <input v-model="form.phone" type="text" @blur="validatePhone" class="joinBox" />
         <span v-if="errors.phone">{{ errors.phone }}</span>
       </div>
-      <button type="joinSubmit">회원가입</button>
+      <!-- 아이디 입력창 -->
+      <div>
+        <label class="joinLabel">아이디</label>
+        <input v-model="form.userId" type="text" @blur="validateuserId" class="joinBox" />
+        <span v-if="errors.userId">{{ errors.userId }}</span>
+      </div>
+      <!-- <button type="joinSubmit">회원가입</button> -->
     </form>
   </div>
 </template>
@@ -23,19 +29,31 @@ import { reactive } from 'vue'
 
 const form = reactive({
   name: '',
-  phone: ''
+  phone: '',
+  userId: ''
 })
 
+
+// 에러
 const errors = reactive({
-  phone: ''
+  phone: '',
+  userId: ''
 })
 
-const validatePhone = () => {
+// 전화번호 유효성 검사
+const validatePhone = () => { 
   const regex = /^[0-9]{10,11}$/
   errors.phone = regex.test(form.phone) ? '' : '숫자만 입력해주세요 (10~11자리)'
 }
-
-const submitForm = () => {
-  console.log('제출됨:', form.name)
+// 아이디 유효성 검사
+const validateuserId = () => {
+  const regex = /^[a-zA-Z0-9!@#$%^&*]{4,20}$/
+  errors.userId = regex.test(form.userId) ? '' : '영문, 숫자, 특수문자 포함 4자 이상'
 }
+
+// const submitForm = () => {
+//   console.log('제출됨:', form.name)
+// }
+
+//초기화 넣어두기
 </script>
