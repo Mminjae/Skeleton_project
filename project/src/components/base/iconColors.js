177c8b409609035d20cbceb9b0c42fa8ada03644
LeeEunmi <eunmilee9109.1@gmail.com>
2025-05-08 19:10:07 +0900

    iconColors.js 추가하기 -> 도넛 그래프 색

---
+++ project/src/components/base/iconColors.js
@@ -0,0 +1,17 @@
+export const iconColors = {
+  // 지출
+  saving: "#5D45DB",
+  foodcost: "#D1B7FF",
+  transport: "#9176E0",
+  culture: "#B9A8F0",
+  essentials: "#D5D7F2",
+  shopping: "#CFC2FA",
+  miscExpense: "#E4DCFF",
+
+  // 수업
+  salary: "#8660D1",
+  finance: "#D3CCF5",
+  allowance: "#A698EB",
+  carryover: "#8D92F2",
+  miscIncome: "#C1BBE6"
+}
